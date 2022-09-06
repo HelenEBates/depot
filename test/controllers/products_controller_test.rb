45ff154c9724c512ff02471a2b2cb1b9939eb0a8
Helen Bates <h.e.dyson@hotmail.com>
2022-09-06 12:45:47 +0100

    Add model associations between Carts and Products
    A callback is added using a private method to makes sure that when
    a product is a line item in a cart, it cannot be deleted.
    
    A test in the controller has been added too.

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -36,7 +36,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_select 'td.description', /Ruby is the fastest growing/
     assert_select 'td.description h1', 'Programming Ruby 1.9'
   end
-  
+
   test "#index has a link to show the product which redirects the user to the show page" do
     get products_url
     assert_select 'td.actions ul li a', 'Show'
@@ -68,6 +68,14 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should update product" do
     patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
+  end
+
+  test "can't delete product in cart" do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
   end
 
   test "should destroy product" do
