c4e7fef7218f6a7c7e7c6ea650f4afd4e93af8df
Helen Bates <h.e.dyson@hotmail.com>
2022-09-06 10:38:40 +0100

    Add skeleton files created by generating the Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_29_101012) do
+ActiveRecord::Schema.define(version: 2022_09_06_093248) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
