8b5ac5c9f0ae6cbdbe2c4516f69eb8ce97b2313e
Helen Bates <h.e.dyson@hotmail.com>
2022-09-07 12:03:42 +0100

    Add a count of visits to the homepage
    Add a private method in the store controller which counts the number
    of visits made to the store.
    The number of visits is displayed in the view, this probably needs
    refactoring, some styling has been applied.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,18 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    store_visit_counter
+  end
+
+  private
+
+  def store_visit_counter
+    if session[:counter].nil?
+      session[:counter] = 1
+      @count = session[:counter]
+    else
+      session[:counter] += 1
+      @count = session[:counter]
+    end
   end
 end
