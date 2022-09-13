91a11eec5758a4290ab7e8d9e311a7d0ac548d90
Helen Bates <h.e.dyson@hotmail.com>
2022-09-13 11:41:47 +0100

    Add unit tests for add_product method in Cart model
    This is a refactor of earlier work adding Unit tests for the
    add_product method in Cart model.
    Some tests have been removed as they were just understanding how fixtures
    work, in the near future FactoryBot will be added and will replace
    fixtures.
    
    A change to one of the controller tests will also be made as the fixures
    are all loaded for each test any changes mean updates to unrelated tests
    are sometimes required.

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -4,7 +4,25 @@ class CartTest < ActiveSupport::TestCase
   fixtures :carts
   fixtures :line_items
   fixtures :products
+
   test "total_price calculates the total price of all line_items in the cart" do
     assert_in_delta 85.99, carts(:two).total_price
   end
+
+  test "add_products returns a line_item instance" do
+    assert_instance_of LineItem, carts(:one).add_product(products(:docker))
+  end
+
+  test "add_product returns quantity of 2 when line_item with that procuct id already exists" do
+    # line_item fixture with product Ruby is present with a quanity of 1.
+    line_item = carts(:one).add_product(products(:ruby))
+    assert_equal 2, line_item.quantity
+  end
+
+  test "add_product builds a line item when one does not already exist in the cart with the same product id" do
+    # line_item fixture with product Rails does not exist.
+    line_item = carts(:one).add_product(products(:rails))
+    assert_equal 1, line_item.quantity
+  end
+
 end
