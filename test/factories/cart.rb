9b6c0c2f13404c1bdfb83ebfacd90c93d8210486
Helen Bates <h.e.dyson@hotmail.com>
2022-09-14 15:13:21 +0100

    Remove Unnecessary method from Carts factory

--- test/factories/cart.rb
+++ test/factories/cart.rb
@@ -2,9 +2,3 @@ FactoryBot.define do
   factory :cart do
   end
 end
-
-def cart_with_line_items(line_items_in_cart: 2)
-  FactoryBot.create(:cart) do |cart|
-    FactoryBot.create_line_items_in_cart(:line_items, line_items_in_cart, cart: cart)
-  end
-end
