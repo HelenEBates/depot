5ba8260310ca5e06ee91d71f3810865af37bde3a
Helen Bates <h.e.dyson@hotmail.com>
2022-09-15 15:13:56 +0100

    Fix pay_type.jsx file

--- app/javascript/packs/pay_type.jsx
+++ app/javascript/packs/pay_type.jsx
@@ -1,24 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import PropTypes from 'prop-types'
-// import PayTypeSelector from 'PayTypeSelector'
-
-
-const Payment = props => (
-  <div>Hello customer {props.name}!</div>
-)
-
-Payment.defaultProps = {
-  name: 'David'
-}
-
-Payment.propTypes = {
-  name: PropTypes.string
-}
+import PayTypeSelector from 'PayTypeSelector'
 
 document.addEventListener('turbolinks:load', () => {
-  ReactDOM.render(
-    <Payment name="Pay Type" />,
-    document.body.appendChild(document.createElement('h1')),
-  )
-})
+  var element = document.getElementById("pay-type-component");
+  ReactDOM.render(<PayTypeSelector />, element);
+});
