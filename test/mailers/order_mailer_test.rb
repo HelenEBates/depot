09e2838b80e886fc1ec113ed1c9944ed63273f5f
Helen Bates <h.e.dyson@hotmail.com>
2022-09-20 17:57:38 +0100

    Add tests for Mailers
    Tests have been added using FactoryBot, the shipped method has not been
    implemented yet but is added in the book for Iteration I1

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -1,20 +1,30 @@
 require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
-  test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+  setup do
+    @product = FactoryBot.create(:product)
+    @order = FactoryBot.create(:order)
+    # cart will have been destroyed by this point in Orders#Create
+    @line_item = FactoryBot.create(:line_item, cart_id: nil, product_id: @product.id, order_id: @order.id)
   end
 
+  test "An email is created but not sent after a User Order has been saved" do
+    @mail = OrderMailer.received(@order)
+    assert_equal "Pragmatic Store Order Confirmation", @mail.subject
+    assert_equal [@order.email], @mail.to
+    assert_equal ["depot@example.com"], @mail.from
+    assert_match "Order Received!", @mail.body.encoded
+    assert_match @product.title, @mail.body.encoded
+  end
+
+  # This method has not been implemented yet - title will be updated when the method is implemented
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    @mail = OrderMailer.shipped(@order)
+    assert_equal "Pragmatic Store Order Shipped", @mail.subject
+    assert_equal [@order.email], @mail.to
+    assert_equal ["depot@example.com"], @mail.from
+    assert_match "Pragmatic Order Shipped", @mail.body.encoded
+    assert_match @product.title, @mail.body.encoded
   end
 
 end
