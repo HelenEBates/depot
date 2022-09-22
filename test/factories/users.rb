9e21b58f8dfca5cebf6f10cdb254cb51f2c5d386
Helen Bates <h.e.dyson@hotmail.com>
2022-09-22 10:37:45 +0100

    Add BCrypt for User authentication
    The BCrypt gem has been added and used in the conventional way by adding
    has_secure_password to the User model. Validations have been added to
    the User model and tested.
    
    A factory for Users uses BCrypt to generate passwords. The default cost
    has been reduced from the default of 10 to 4, which makes the tests run
    a slightly faster.

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name { "MyString" }
-    password { "" }
+    sequence(:name) { |n| "customer-name-#{n}" }
+    password_digest { BCrypt::Password.create('secret', cost: 4) }
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "User must have a name" do
+    user = FactoryBot.build(:user, name: nil)
+    assert user.invalid?
+    assert_equal ["can't be blank"], user.errors[:name]
+  end
+
+  test "User name must be unique" do
+    user = FactoryBot.create(:user, name: "Grizzly Bear")
+    user1 = FactoryBot.build(:user, name: "Grizzly Bear")
+    assert user1.invalid?
+    assert_equal ["has already been taken"], user1.errors[:name]
+  end
+
+  test "User must have a password" do
+    user = FactoryBot.build(:user, password: nil)
+    assert user.invalid?
+    assert_equal ["can't be blank"], user.errors[:password]
+  end
 end
