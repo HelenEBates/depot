f2e45c5e0abac324ef8db6d4cca96403235e32f5
Helen Bates <h.e.dyson@hotmail.com>
2022-09-22 16:46:09 +0100

    Restrict access for users who are not logged in
    This commit adds a before_action in ApplicationController to filter out
    users who are not logged in. The before_action is skipped for
    controller actions which need to be accessible to non logged in users.
    
    An AuthenticationHelpers module has been added to the test_helper and
    included in both ActionDispatch::IntegrationTest and
    ActionDispatch::SystemTestCase so that users are logged in as
    necessary for the tests without having to setup to log the user in in
    each test requiring it.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,33 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  module AuthenticationHelpers
+    def login_as(user)
+      if respond_to? :visit
+        visit login_url
+        fill_in :name, with: user.name
+        fill_in :password, with: 'secret'
+        click_on 'Login'
+      else
+        post login_url, params: { name: user.name, password: 'secret' }
+      end
+    end
 
-  # Add more helper methods to be used by all tests here...
+    def logout
+      delete logout_url
+    end
+
+    def setup
+      user = FactoryBot.create(:user)
+      login_as(user)
+    end
+
+    class ActionDispatch::IntegrationTest
+      include AuthenticationHelpers
+    end
+
+    class ActionDispatch::SystemTestCase
+      include AuthenticationHelpers
+    end
+  end
 end
