a6504c1a2eb679a1eb95e7ec0bfcfd53f37dd85e
Helen Bates <h.e.dyson@hotmail.com>
2022-09-28 12:26:11 +0100

    Update the SupportMailbox and add an initial test
    This commit adds a recent_order scope to find the most recent Order
    with an email matching the email address from the email, if there is one.
    
    It also creates and instance of SupportRequest using the Mail object.
    
    The test could be updated to use FactoryBot.

--- test/mailboxes/support_mailbox_test.rb
+++ test/mailboxes/support_mailbox_test.rb
@@ -1,11 +1,18 @@
 require "test_helper"
 
 class SupportMailboxTest < ActionMailbox::TestCase
-  # test "receive mail" do
-  #   receive_inbound_email_from_mail \
-  #     to: '"someone" <someone@example.com>',
-  #     from: '"else" <else@example.com>',
-  #     subject: "Hello world!",
-  #     body: "Hello?"
-  # end
+  test "A SupportRequest is created when a supporrt email is received" do
+    receive_inbound_email_from_mail(
+      to: "support@example.com",
+      from: "me@gmail.com",
+      subject: "Please help",
+      body: "I am having trouble with checkout"
+    )
+
+    support_request = SupportRequest.last
+    assert_equal "me@gmail.com", support_request.email
+    assert_equal "Please help", support_request.subject
+    assert_equal "I am having trouble with checkout", support_request.body
+    assert_nil support_request.order
+  end
 end
